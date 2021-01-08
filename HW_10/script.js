ab811691c0d58b0a7618a6625f5ed5709ced1070
Anastasiia Shliaieva <stacyme2522@gmail.com>
2021-01-08 19:37:42 +0200

    done

---
+++ HW_10/script.js
@@ -0,0 +1,48 @@
+'use strict'
+
+let phonebook = document.querySelector('#phonebook');
+let contactNameEl = document.querySelector('#contactName');
+let contactPhoneEl = document.querySelector('#contactPhone');
+let contactEmailEl = document.querySelector('#contactEmail');
+let addContactEl = document.querySelector('#addContact');
+
+addContactEl.addEventListener('click', addOnBtnClick);
+
+function addOnBtnClick() {
+    if (contactNameEl.value && contactPhoneEl.value && contactEmailEl.value) {
+        addNewTask();
+        clearInput();
+    } else {
+        alert('Fill in all contact details');
+    };
+};
+
+function addNewTask() {
+    let phonebookEl = document.createElement('ul');
+    phonebookEl.className = 'userPhonebookList';
+    phonebook.append(phonebookEl);
+    phonebookEl.appendChild(addPhonebookButton())
+    phonebookEl.appendChild(addPhonebookEl(contactNameEl));
+    phonebookEl.appendChild(addPhonebookEl(contactPhoneEl));
+    phonebookEl.appendChild(addPhonebookEl(contactEmailEl));
+    
+    phonebookEl.firstChild.addEventListener ('click', () => phonebookEl.remove()) ;
+};
+
+function addPhonebookEl(el) {
+    let contactEl = document.createElement('li');
+    contactEl.textContent = el.value;
+    return contactEl;    
+};
+
+function addPhonebookButton() {
+    let removeButton = document.createElement('button');
+    removeButton.textContent = 'REMOVE';
+    return removeButton;
+};
+
+function clearInput() {
+    contactNameEl.value = '';
+    contactPhoneEl.value = '';
+    contactEmailEl.value = '';
+};
--- HW_9/script.js
+++ HW_9/script.js
@@ -4,11 +4,38 @@ let userToDoListEl = document.querySelector('#userToDoList');
 let inputFieldEl = document.querySelector('#inputField');
 let addToDoButtonEl = document.querySelector('#addToDo');
 
-addToDoButtonEl.addEventListener('click', onBtnClick);
+addNewTask('New task')
 
-function onBtnClick() {
-    let liEl = document.createElement('li');
-    liEl.innerText = inputFieldEl.value;
-    userToDoListEl.append(liEl);
-    inputFieldEl.value = null;
+addToDoButtonEl.addEventListener('click', addOnBtnClick);
+
+function addOnBtnClick() {
+    if (inputFieldEl.value) {
+        addNewTask(inputFieldEl.value)
+        clearInput()
+    }
 };
+
+function addNewTask(title) {
+    let taskEl = document.createElement('li');
+    taskEl.textContent = title;
+    userToDoListEl.append(taskEl);
+}
+
+function clearInput() {
+    inputFieldEl.value = '';
+}
+
+// my HW
+
+// let userToDoListEl = document.querySelector('#userToDoList');
+// let inputFieldEl = document.querySelector('#inputField');
+// let addToDoButtonEl = document.querySelector('#addToDo');
+
+// addToDoButtonEl.addEventListener('click', onBtnClick);
+
+// function onBtnClick() {
+//     let taskEl = document.createElement('li');
+//     taskEl.innerText = inputFieldEl.value;
+//     userToDoListEl.append(taskEl);
+//     inputFieldEl.value = null;
+// };
